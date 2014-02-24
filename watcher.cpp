79198bd1af41130479803d42542460448dd67a1b
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2014-02-24 21:55:41 +0100

    Add accept_path() method to check if a path should be accepted according to the exclude filters.

--- watcher.cpp
+++ watcher.cpp
@@ -1,5 +1,6 @@
 #include "watcher.h"
 #include "fsw_exception.h"
+#include <cstdlib>
 
 watcher::watcher(vector<string> paths_to_watch, EVENT_CALLBACK callback) :
     paths(paths_to_watch), callback(callback)
@@ -25,6 +26,53 @@ void watcher::set_recursive(bool recursive)
   this->recursive = recursive;
 }
 
+void watcher::set_exclude(const vector<string> &exclusions)
+{
+  for (string exclusion : exclusions)
+  {
+    regex_t regex;
+    if(::regcomp(&regex, exclusion.c_str(), 0))
+    {
+      string err = "An error occurred during the compilation of " + exclusion;
+      throw new fsw_exception(err);
+    }
+
+    exclude_regex.push_back(regex);
+  }
+}
+
+bool watcher::accept_path(const string &path)
+{
+  for (auto re : exclude_regex)
+  {
+    if(::regexec(&re, path.c_str(), 0, nullptr, 0) == 0)
+    {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+bool watcher::accept_path(const char *path)
+{
+  for (auto re : exclude_regex)
+    {
+      if(::regexec(&re, path, 0, nullptr, 0) == 0)
+      {
+        return false;
+      }
+    }
+
+    return true;
+}
+
 watcher::~watcher()
 {
+  for (auto re : exclude_regex)
+  {
+    ::regfree(&re);
+  }
+
+  exclude_regex.clear();
 }
--- watcher.h
+++ watcher.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <string>
+#include <regex.h>
 #include "event.h"
 
 using namespace std;
@@ -16,14 +17,22 @@ public:
   virtual ~watcher();
   void set_latency(double latency);
   void set_recursive(bool recursive);
+  void set_exclude(const vector<string> &exclusions);
 
   virtual void run() = 0;
+
+protected:
+  bool accept_path(const string &path);
+  bool accept_path(const char *path);
 
 protected:
   vector<string> paths;
   EVENT_CALLBACK callback;
   double latency = 1.0;
   bool recursive = false;
+
+private:
+  vector<regex_t> exclude_regex;
 };
 
 #endif  /* FSW__WATCHER_H */
