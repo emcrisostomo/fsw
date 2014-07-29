196cca23b49acbf1b01dfa4b071c2e5d65684ad3
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2014-07-29 22:52:51 +0200

    Add method skeletons.

--- libfsw/libfsw.cpp
+++ libfsw/libfsw.cpp
@@ -1,11 +1,64 @@
 #include <iostream>
+#include <mutex>
+#include <time.h>
+#include <stdlib.h>
 #include "libfsw.h"
+#include "libfsw_map.h"
 
 using namespace std;
 
-static bool vFlag = false;
-
-bool is_verbose()
+typedef struct FSW_SESSION
 {
-  return vFlag;
+  bool is_verbose;
+} FSW_SESSION;
+
+static bool srand_initialized = false;
+static fsw_hash_map<FSW_HANDLE, FSW_SESSION> sessions;
+static std::mutex session_mutex;
+static FSW_THREAD_LOCAL unsigned int last_error;
+
+FSW_HANDLE fsw_init_session()
+{
+  std::lock_guard<std::mutex> session_lock(session_mutex);
+
+  if (!srand_initialized)
+  {
+    srand(time(nullptr));
+    srand_initialized = true;
+  }
+
+  int handle;
+
+  do
+  {
+    handle = abs(rand());
+  }
+  while (sessions.find(handle) != sessions.end());
+
+  FSW_SESSION session;
+
+  // TODO: configure session
+  sessions[handle] = session;
+
+  return handle;
+}
+
+unsigned int fsw_last_error()
+{
+  return last_error;
+}
+
+bool fsw_is_verbose()
+{
+  return false;
+}
+
+void fsw_set_verbose(FSW_HANDLE handle, bool verbose)
+{
+  // TODO
+}
+
+void fsw_add_filter(FSW_HANDLE handle, char * regex)
+{
+
 }
--- libfsw/libfsw.h
+++ libfsw/libfsw.h
@@ -1,10 +1,24 @@
-#ifdef __cplusplus
+#ifndef LIBFSW_H
+#  define LIBFSW_H
+
+#  ifdef __cplusplus
 extern "C"
 {
-#endif
+#  endif
 
-bool is_verbose();
+  typedef unsigned int FSW_HANDLE;
 
-#ifdef __cplusplus
+#  define FSW_INVALID_HANDLE -1
+  // TODO Implement Autoconf check
+#  define FSW_THREAD_LOCAL /* thread_local */
+
+  FSW_HANDLE fsw_init_session();
+  unsigned int fsw_last_error();
+  bool fsw_is_verbose();
+  void fsw_set_verbose(FSW_HANDLE handle, bool verbose);
+
+#  ifdef __cplusplus
 }
-#endif
+#  endif
+
+#endif /* LIBFSW_H */
