80acbe56ebb7b2122b88f73ae15f9486979d45f3
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2014-08-02 23:52:35 +0200

    Move filter structs and enums in a separate C friendly header.

---
+++ libfsw/filter.h
@@ -0,0 +1,34 @@
+/* 
+ * Copyright (C) 2014, Enrico M. Crisostomo
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef FSW__FILTER_H
+#  define FSW__FILTER_H
+
+#  include <string>
+
+enum filter_type
+{
+  filter_include,
+  filter_exclude
+};
+
+typedef struct monitor_filter
+{
+  std::string text;
+  filter_type type;
+} monitor_filter;
+
+#endif  /* FSW__FILTER_H */
