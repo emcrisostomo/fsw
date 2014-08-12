d63c496fc1cc2b02bcf5da10fbd8daa3c891ebb0
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2014-08-12 14:53:20 +0200

    Move error definitions to a separate header to share it between C and C++ code.

--- libfsw/c/libfsw.h
+++ libfsw/c/libfsw.h
@@ -4,29 +4,12 @@
 #  include "cevent.h"
 #  include "cmonitor.h"
 #  include "cfilter.h"
+#  include "error.h"
 
 #  ifdef __cplusplus
 extern "C"
 {
 #  endif
-
-  // Error codes
-#  define FSW_OK                            0
-#  define FSW_ERR_SESSION_UNKNOWN           (1 << 0)
-#  define FSW_ERR_MONITOR_ALREADY_EXISTS    (1 << 1)
-#  define FSW_ERR_MEMORY                    (1 << 2)
-#  define FSW_ERR_UNKNOWN_MONITOR_TYPE      (1 << 3)
-#  define FSW_ERR_CALLBACK_NOT_SET          (1 << 4)
-#  define FSW_ERR_PATHS_NOT_SET             (1 << 5)
-#  define FSW_ERR_UNKNOWN_MONITOR           (1 << 6)
-#  define FSW_ERR_MISSING_CONTEXT           (1 << 7)
-#  define FSW_ERR_INVALID_PATH              (1 << 8)
-#  define FSW_ERR_INVALID_CALLBACK          (1 << 9)
-#  define FSW_ERR_INVALID_LATENCY           (1 << 10)
-#  define FSW_ERR_MONITOR_ALREADY_RUNNING   (1 << 11)
-#  define FSW_ERR_STALE_MONITOR_THREAD      (1 << 12)
-#  define FSW_ERR_THREAD_FAULT              (1 << 13)
-#  define FSW_ERR_UNSUPPORTED_OPERATION     (1 << 14)
 
   typedef unsigned int FSW_HANDLE;
 
